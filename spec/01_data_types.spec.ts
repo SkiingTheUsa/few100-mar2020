af3e816de94bc344a941466aae75ad30d3e636ee
Craig McCartney <craig.a.mccartney@gmail.com>
2020-03-26 19:53:09 +0000

    done with day 1

---
+++ spec/01_data_types.spec.ts
@@ -0,0 +1,11 @@
+describe('declaring variables in typescript', () => {
+    describe('typing', () => {
+        it('implicity typed variables', () => {
+            let name = 'Joe';
+            name = 'Ray';
+
+
+        });
+    });
+});
+
--- spec/demo_with_typescript.spec.ts
+++ spec/demo_with_typescript.spec.ts
@@ -2,9 +2,20 @@ import { add } from './utils';
 
 describe('writing specs in typescript', () => {
     it('is easy', () => {
-        expect(true).toBe(false);
+        expect(true).toBe(true);
     });
     it('can add', () => {
-        expect(add(2, 2)).toBe(5);
+        expect(add(2, 2)).toBe(4);
+    });
+});
+describe('writying a basic spec', () => {
+    it('an example of this', () => {
+        const myName = 'Craig';
+        expect(myName.length).toBe(5);
+    });
+});
+describe('using the snippets', () => {
+    it('pretty cool', () => {
+
     });
 });
